8b8fea59255e2068fcbcc5946a56942c2fda2e6d
snehasamuel <snehasamuel297@gmail.com>
2022-05-20 05:03:27 +0530

    Login branch created

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Header = () => {
   return (
     <div>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-success">
 
 <Link class="navbar-brand" to="/add">BOOK APP</Link>
 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,41 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Addbook from './Addbook'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div>
+    
+        <div classNAme="container">
+    <div classNAme="row">
+        <div classNAme="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+            <div classNAme="row g-3">
+                <div classNAme="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" classNAme="form-label">UserName</label>
+                    <input type="text" classNAme="form-control"/>
+                </div>
+                <div classNAme="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" classNAme="form-label">Password</label>
+                    <input type="text" classNAme="form-control"/>
+                </div>
+                <div classNAme="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <button classNAme="btn btn-success" onClick={<Addbook/>}>LOGIN</button>
+                </div>
+          
+            <div classNAme="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+               <Link className='nav-link' to="/register">New User ?? Click Here !!</Link>
+            </div>
+            
+
+        </div>
+    </div>
+</div>
+    </div>
+    </div>
   )
 }
 
